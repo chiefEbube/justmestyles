b383fbb909c1c1ace918f4575cefd5651a6db6ca
chiefebube <ebubechi.anyanwu@yahoo.com>
2023-11-16 12:50:09 +0100

    more routes

---
+++ src/routes/navigation/navigaton.component.jsx
@@ -0,0 +1,26 @@
+import { Outlet, Link } from "react-router-dom"
+import jmsLogo from '../../assets/JMS.png'
+
+import './navigation.styles.scss'
+
+const Navigation = () => {
+    return (
+        <>
+            <div className="navigation">
+                <Link className="logo-container" to='/'>
+                    <img
+                        src={jmsLogo}
+                        className="logo"/>
+                </Link>
+                <div className="nav-links-container">
+                    <Link className="nav-link" to='/shop'>
+                        SHOP
+                    </Link>
+                </div>
+            </div>
+            <Outlet />
+        </>
+    )
+}
+
+export default Navigation

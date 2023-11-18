7023d7248b30b6fe77d8e301bbb0499c2c2b2ec5
chiefebube <ebubechi.anyanwu@yahoo.com>
2023-11-18 06:04:00 +0100

    sign up form cont'd

--- src/components/sign-up-form/sign-up-form.component.jsx
+++ src/components/sign-up-form/sign-up-form.component.jsx
@@ -1,19 +1,66 @@
-const SignUpForm = ()=>{
-    return(
+import { useState } from "react"
+
+const defaultFormFields = {
+    displayName: '',
+    email: '',
+    password: '',
+    confirmPassword: ''
+}
+
+const SignUpForm = () => {
+    const [formFields, setFormFields] = useState(defaultFormFields)
+    const { displayName, email, password, confirmPassword } = formFields
+
+    console.log(formFields)
+
+    const handleChange = (event) => {
+        const {name, value} = event.target
+
+        setFormFields({
+            ...formFields,
+            [name]: value
+        })
+
+
+    }
+
+    return (
         <div>
             <h1>Sign up  with email and password</h1>
-            <form onSubmit={()=>{}}>
+            <form onSubmit={() => { }}>
                 <label>Display Name</label>
-                <input type="text" required/>
+                <input
+                    required name="displayName"
+                    type="text"
+                    onChange={handleChange}
+                    value={displayName}
+                />
 
                 <label>Email</label>
-                <input type="email" required/>
+                <input
+                    required name="email"
+                    type="email"
+                    onChange={handleChange}
+                    value={email}
+                />
 
                 <label>Password</label>
-                <input type="password" required/>
+                <input
+                    required
+                    name="password"
+                    type="password"
+                    onChange={handleChange}
+                    value={password}
+                />
 
                 <label>Confirm password</label>
-                <input type="password" required/>
+                <input
+                    required
+                    name="confirmPassword"
+                    type="password"
+                    onChange={handleChange}
+                    value={confirmPassword}
+                />
                 <button type="submit">Submit</button>
             </form>
         </div>
--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -1,9 +1,10 @@
 import { initializeApp } from 'firebase/app';
-import { 
+import {
     getAuth,
     signInWithRedirect,
     signInWithPopup,
-    GoogleAuthProvider 
+    GoogleAuthProvider,
+    createUserWithEmailAndPassword
 } from 'firebase/auth';
 import {
     getFirestore,
@@ -34,24 +35,32 @@ export const signInWithGooglePopup = () => signInWithPopup(auth, googleProvider)
 export const signInWithGoogleRedirect = () => signInWithRedirect(auth, googleProvider)
 export const db = getFirestore()
 
-export const createUserDocumentFromAuth = async (userAuth) =>{
+export const createUserDocumentFromAuth = async (userAuth) => {
+    if (!userAuth) return;
+
     const userDocRef = doc(db, 'users', userAuth.uid)
     const userSnapshot = await getDoc(userDocRef)
 
-    if(!userSnapshot.exists()){
+    if (!userSnapshot.exists()) {
         const { displayName, email } = userAuth
         const createdAt = new Date()
 
-        try{
+        try {
             await setDoc(userDocRef, {
                 displayName,
                 email,
                 createdAt
             })
-        } catch (error){
+        } catch (error) {
             console.log('error creating the user', error.message)
         }
     }
 
     return userDocRef
+}
+
+export const createAuthUserWithEmailAndPassword = async (email, password) => {
+    if (!email || !password) return;
+
+    return createUserDocumentFromAuth(auth, email, password)
 }

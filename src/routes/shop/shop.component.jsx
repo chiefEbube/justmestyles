f3920a41df53324f00141d83690d0371a6d1f70a
chiefebube <ebubechi.anyanwu@yahoo.com>
2023-12-05 14:15:45 +0100

    added add-to-cart feature

--- src/routes/shop/shop.component.jsx
+++ src/routes/shop/shop.component.jsx
@@ -5,13 +5,13 @@ import ProductCard from "../../components/product-card/product-card.component"
 
 import './shop.styles.scss'
 const Shop = () => {
-    const {products} = useContext(ProductsContext)
+    const { products } = useContext(ProductsContext)
 
     return (
         <div className="products-container">
             {products.map((product) => (
-            <ProductCard key={product.id} product={product}/>
-        ))}
+                <ProductCard key={product.id} product={product} />
+            ))}
         </div>
     )
 }

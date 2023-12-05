ec404f15a5d8e67f78d9f15a9cae2779f052d04b
chiefebube <ebubechi.anyanwu@yahoo.com>
2023-12-05 16:10:17 +0100

    fixed typo

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import { Routes, Route } from "react-router-dom";
 import Home from "./routes/home/home.component";
-import Navigation from "./routes/navigation/navigaton.component";
+import Navigation from "./routes/navigation/navigation.component";
 import Authentication from "./routes/authentication/authentication.components";
 import Shop from "./routes/shop/shop.component";
 
